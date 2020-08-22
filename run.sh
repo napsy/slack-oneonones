4865937d2b874134c074527f64bbf8334b7ab4ae
Luka Napotnik <luka@zeta.si>
2020-08-23 00:08:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 export SLACK_TOKEN=<INSERT YOUR SLACK TOKEN HERE>
 while :
 do
-	./remid -o your_company_email@example.com
+	./slack-oneonones -o your_company_email@example.com
 	sleep 600 # sleep for 10 minutes
 done
